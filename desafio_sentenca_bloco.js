506f375f4369a61be7e2bee4ef34ac672ad5b1d0
unknown <lucas.matheusgorni@hotmail.com>
2023-05-14 18:57:40 -0300

    Curso Udemy junto do botcampDio JavaScript

---
+++ desafio_sentenca_bloco.js
@@ -0,0 +1,17 @@
+//Sequencia Impar
+{
+    console.log(1);
+    console.log(3);
+    console.log(5); //Bloco de Codigos Impar
+    console.log(7);
+    console.log(9);
+}
+
+//Sequencia Par 
+{
+    console.log(2);
+    console.log(4);
+    console.log(6); //Bloco de Codigos Par
+    console.log(8);
+    console.log(10);
+}
---
+++ typeof_js.js
@@ -0,0 +1,9 @@
+// Exemplos para Saber tipo de variavel\
+
+
+let idade = 25; // JS Resultado de Inteiro e sempre Number;
+let nome = "Fulano";
+let data = 17.04; // JS Resultado de .Flutuante e sempre Number;
+let estaChovendo = true; // or false
+
+console.log(typeof idade, typeof nome, typeof data, typeof estaChovendo);
