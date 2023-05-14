5b7666b9e14d61d5ba8a7e8a9bd0016b4c0f7ced
unknown <lucas.matheusgorni@hotmail.com>
2023-05-14 19:03:13 -0300

    Curso Udemy JS junto de botcampdio

--- typeof_js.js
+++
@@ -1,9 +0,0 @@
-// Exemplos para Saber tipo de variavel\
-
-
-let idade = 25; // JS Resultado de Inteiro e sempre Number;
-let nome = "Fulano";
-let data = 17.04; // JS Resultado de .Flutuante e sempre Number;
-let estaChovendo = true; // or false
-
-console.log(typeof idade, typeof nome, typeof data, typeof estaChovendo);
